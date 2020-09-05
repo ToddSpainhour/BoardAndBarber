a439764c8e01b7f452e6ce6e994c2dfb3671f217
Todd Spainhour <toddspainhour@gmail.com>
2020-09-05 17:42:16 -0500

    Add project files.

---
+++ BoardAndBarber/Controllers/CustomersController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BoardAndBarber.Models;
+using BoardAndBarber.Data;
+
+namespace BoardAndBarber.Controllers
+{
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+
+        CustomerRepository _repo;
+
+        [HttpPost]
+        public IActionResult CreateCustomer(Customer customer)
+        {
+            var repo = new CustomerRepository();
+                repo.Add(customer);
+
+            return Created($"/api/customers/{customer.Id}", customer);
+        }
+
+
+
+        [HttpGet]
+            public IActionResult GetAllCustomers()
+        {
+            var repo = new CustomerRepository();
+            var allCustomers = repo.GetAll();
+
+            return Ok(allCustomers);
+        }
+
+
+
+        [HttpPut("{id}")]
+        public IActionResult updateCustomer (int id, Customer customer)
+        {
+            var updatedCustomer = _repo.Update(id, customer);
+
+            return Ok(updatedCustomer);
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCustomer(int id)
+        {
+            if (_repo.GetById(id) == null)
+            {
+                return NotFound();
+            }
+
+            _repo.Remove(id);
+
+            return Ok();
+
+        }
+
+
+
+
+    }
+}
---
+++ BoardAndBarber/Data/CustomerRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BoardAndBarber.Models;
+
+namespace BoardAndBarber.Data
+{
+    public class CustomerRepository
+    {
+
+
+
+        static List<Customer> _customers = new List<Customer>();
+
+
+
+        public void Add(Customer customerToAdd)
+        {
+            var newId = 0;
+            if (_customers.Count > 0)
+            {
+            newId = _customers.Select(p => p.Id).Max() + 1;
+            }
+            customerToAdd.Id = newId;
+
+            _customers.Add(customerToAdd);
+        }
+
+
+
+        public List<Customer> GetAll()
+        {
+            return _customers;
+        }
+
+
+
+        public Customer GetById(int id)
+        {
+            return _customers.FirstOrDefault(c => c.Id == id);
+        }
+
+
+
+        public Customer Update (int id, Customer customer)
+        {
+            var customerToUpdate = GetById(id);
+
+            customerToUpdate.Birthday = customer.Birthday;
+            customerToUpdate.FavoriteBarber = customer.FavoriteBarber;
+            customerToUpdate.Notes = customer.Notes;
+            customerToUpdate.Name = customer.Name;
+
+            return customerToUpdate;
+        }
+
+
+
+        public void Remove (int id)
+        {
+            var customerToDelete = GetById(id);
+
+            _customers.Remove(customerToDelete);
+        }
+
+    }
+}
